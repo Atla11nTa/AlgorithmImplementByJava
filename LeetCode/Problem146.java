6b27f47e713f4dbcf3de6836e2071af913b0fff3
Atla11nTa <1518585834@qq.com>
2020-07-20 18:07:22 +0800

    "2020/7/20/18:00"

--- LeetCode/Problem146.java
+++ LeetCode/Problem146.java
@@ -1,19 +1,100 @@
 package LeetCode;
 
-import java.util.LinkedList;
+import java.util.HashMap;
 
 public class Problem146 {
-    LinkedList<>
+    private HashMap<Integer, Node> hashMap;
+    private Node head;
+    private Node tail;
+    private int size;
+    private int capacity;
 
     public Problem146(int capacity) {
-
+        this.hashMap = new HashMap<>(capacity);
+        this.size = 0;
+        this.capacity = capacity;
+        this.head = new Node(-1, -1);
+        this.tail = new Node(-1, -1);
+        head.pre = null;
+        head.next = tail;
+        tail.pre = head;
+        tail.next = null;
     }
 
     public int get(int key) {
-
+        Node node = hashMap.get(key);
+        if (node == null) {
+            return -1;
+        } else {
+            Node pre = node.pre;
+            Node next = node.next;
+            pre.next = next;
+            next.pre = pre;
+            //移到链表尾部
+            nodeMoveToTail(node);
+            return node.value;
+        }
     }
 
     public void put(int key, int value) {
+        if (capacity == 0) {
+            return;
+        }
+        //如果已经有了.
+        if (hashMap.containsKey(key)) {
+            Node node = hashMap.get(key);
+            node.value = value;
+            hashMap.put(key, node);
+            Node pre = node.pre;
+            Node next = node.next;
+            pre.next = next;
+            next.pre = pre;
+            //移到链表尾部
+            nodeMoveToTail(node);
+        } else {
+            // 容量达到上限.
+            if (size == capacity) {
+                // 把原来的头部第一个节点删除
+                hashMap.remove(head.next.key);
+                Node headNext = head.next.next;
+                head.next = headNext;
+                headNext.pre = head;
+                size--;
+            }
+            size++;
+            //在尾部插入
+            Node newNode = new Node(key, value);
+            nodeMoveToTail(newNode);
+            hashMap.put(key, newNode);
+        }
+    }
 
+    //把节点移到尾部
+    private void nodeMoveToTail(Node node) {
+        Node pre = tail.pre;
+        node.next = tail;
+        node.pre = pre;
+        pre.next = node;
+        tail.pre = node;
+    }
+    static class Node {
+        Node pre;
+        Node next;
+        int key;
+        int value;
+
+        Node(int key,int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem146 cache = new Problem146( 1 /* 缓存容量 */ );
+        cache.put(2, 1);
+        cache.get(2);       // 返回  1
+        cache.put(3, 2);
+        cache.get(2);
+        cache.get(3);
     }
 }
--- LeetCode/Problem432.java
+++ LeetCode/Problem432.java
@@ -3,33 +3,4 @@ package LeetCode;
 import java.util.HashMap;
 
 public class Problem432 {
-    HashMap<String,Integer> hashMap;
-    int max;
-    int min;
-    public Problem432() {
-        hashMap = new HashMap<>();
-        max = Integer.MIN_VALUE;
-        min = Integer.MAX_VALUE;
-    }
-
-    public void inc(String key) {
-        hashMap.put(key, hashMap.getOrDefault(key, 0) + 1);
-    }
-
-    public void dec(String key) {
-        int count = hashMap.get(key);
-        if (count == 1) {
-            hashMap.remove(key);
-        } else {
-            hashMap.put(key, count - 1);
-        }
-    }
-
-    public String getMaxKey() {
-
-    }
-
-    public String getMinKey() {
-
-    }
 }
